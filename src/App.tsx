7bb3838f22ca44b9831a31b3c2190d009ec91a2a
abdelrahmangad95@gmail.com <abdelrahmangad95@gmail.com>
2022-06-09 14:38:32 +0200

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,58 +1,24 @@
 import React from 'react';
-import { useState, FC, ChangeEvent, FormEvent } from 'react';
-import { ITask } from './interfaces/ITask';
+import logo from './logo.svg';
 import './App.css';
-import { TodoTask } from './components/TodoTask';
 
-const App: FC = () => {
-  const [task, setTask] = useState();
-  const [taskName, setTaskName] = useState<string>('');
-  const [deadline, setDeadline] = useState<number>(0);
-  const [todoList, setTodoList] = useState<ITask[]>([{ taskName: 'task 1 ', deadline: 4 }]);
-
-  const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
-    if (event.target.name === 'task') {
-      setTaskName(event.target.value);
-    } else {
-      setDeadline(Number(event.target.value));
-    }
-  }
-
-  const addTask = (): void => {
-    setTodoList([...todoList, { taskName, deadline }]);
-    setTaskName('');
-    setDeadline(0);
-  }
-  const completeTask = (taskNameToDelete: string): void => {
-    setTodoList(todoList.filter(task => task.taskName !== taskNameToDelete));
-  }
+function App() {
   return (
     <div className="App">
-      <div className="header">
-        <div className="inputContainer">
-          <input
-            type="text"
-            placeholder="Task..."
-            name="task"
-            value={taskName}
-            onChange={handleChange}
-          />
-          <input
-            type="number"
-            placeholder="Deadline (in Days)..."
-            name="deadline"
-            value={deadline}
-            onChange={handleChange}
-          />
-        </div>
-        <button onClick={addTask} >Add Task</button>
-      </div>
-      <div className="todoList">
-        {todoList.map((task: ITask, key: number) => {
-          return <TodoTask key={key} task={task} completeTask={completeTask} />
-        })}
-      </div>
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
